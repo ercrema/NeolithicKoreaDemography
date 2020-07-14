515cc00c86f229c2cde9594527e593db4706d7c7
Enrico R. Crema <enrico.crema@gmail.com>
2020-07-14 18:09:08 +0100

    ABC posterior predictive check routine

---
+++ abc/laplace_ppcheck.R
@@ -0,0 +1,22 @@
+# Load ABC results and simulation model
+load('../results_images/resABC_laplace.RData')
+load('../data/koreanC14.RData')
+source('./sim_model.R')
+pred.check.size = 100 
+best_fit_uncal.param = res[order(res$euc.uncal)[1:pred.check.size],]
+best_fit_cal.param = res[order(res$euc.cal)[1:pred.check.size],]
+
+ppcheck.uncal = ppcheck.cal =matrix(NA,nrow=length(7000:3000),ncol=pred.check.size)
+
+for (i in 1:pred.check.size)
+{
+  tmp.uncal=sim.model(x=koreaC14,caldates=caldates,bins=bins,a=best_fit_uncal.param$a[i],bl=best_fit_uncal.param$bl[i],br=best_fit_uncal.param$br[i],c=best_fit_uncal.param$c[i],timeRange=c(7000,3000),simonly=TRUE,method='uncalsample')
+  
+  ppcheck.uncal[,i] = tmp.uncal$grid$PrDens
+  
+  tmp.cal=sim.model(x=koreaC14,caldates=caldates,bins=bins,a=best_fit_cal.param$a[i],bl=best_fit_cal.param$bl[i],br=best_fit_cal.param$br[i],c=best_fit_cal.param$c[i],timeRange=c(7000,3000),simonly=TRUE,method='calsample')
+  
+  ppcheck.cal[,i] = tmp.cal$grid$PrDens
+}
+
+save(ppcheck.uncal,ppcheck.cal,file='../results_images/predcheck_results.RData')
