c7fee0d35d5f64eb284e748ea05331b80f3ce2f2
Enrico R. Crema <enrico.crema@gmail.com>
2020-07-29 09:38:11 +0100

    Added CKDE and permutation test for inland vs coast

--- standard_analysis.R
+++ standard_analysis.R
@@ -8,10 +8,24 @@ nsim = 5000 #number of MC iterations for modelTest()
 timeRange = c(7000,3000) #time range of analysis
 ncores = 5 #number of cores
 
-# CKDE
+# CKDE ####
 ckdeNorm = ckde(sdates,timeRange=timeRange,bw=bw,normalised=TRUE)
 
-# Against Theorethical Growth Models:
+# Inland/Coastal Comparison
+costal.index = which(koreaC14$region=='coastal')
+inland.index = which(koreaC14$region=='inland')
+
+sdates.coastal = sampleDates(caldates[costal.index], bins = bins[costal.index], nsim=1000, boot = TRUE, verbose = TRUE)
+sdates.inland = sampleDates(caldates[inland.index], bins = bins[inland.index], nsim=1000, boot = TRUE, verbose = TRUE)
+
+ckdeNorm.coastal = ckde(sdates.coastal,timeRange=timeRange,bw=bw,normalised=TRUE)
+ckdeNorm.inland = ckde(sdates.inland,timeRange=timeRange,bw=bw,normalised=TRUE)
+
+# Permutation Test
+CoastVsInland = permTest(caldates,marks=koreaC14$region,timeRange=timeRange,bins=bins,nsim=nsim,runm=runm)
+
+
+# Against Theoretical Growth Models:
 
 ## Exponential ##
 set.seed(123)
@@ -26,4 +40,4 @@ set.seed(123)
 logFitDens=data.frame(calBP=obs.calBP,PrDens=SSlogis(obs.calBP,coefficients(logFit)[1],coefficients(logFit)[2],coefficients(logFit)[3]))
 LogCheck <- modelTest(caldates,bins=bins,errors=koreaC14$c14error,model="custom",runm=runm,nsim=nsim,ncores=ncores,timeRange=timeRange,predgrid=logFitDens,spdnormalised = TRUE)
 
-save(m1,LogCheck,file='./results_images/result_standard_analysis.RData')
+save(m1,LogCheck,ckdeNorm.coastal,ckdeNorm.inland,CoastVsInland,file='./results_images/result_standard_analysis.RData')
