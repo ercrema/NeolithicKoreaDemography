376484b714da5403b8f5eb82320f8c7838962d83
Enrico R. Crema <enrico.crema@gmail.com>
2020-08-19 11:56:44 +0100

    Added marin20/bchron reassessment of kim 2004 age depth model

--- figures/figurelog.R
+++ figures/figurelog.R
@@ -3,6 +3,10 @@ library(raster)
 library(maptools)
 library(rworldmap)
 library(rcarbon)
+library(Bchron)
+library(coda)
+
+
 load('../data/koreanC14.RData')
 load('../results_images/test_results.RData')
 
@@ -97,9 +101,7 @@ lines(logistic.inland.test$fit,lty=2,lwd=1,col='grey22')
 title('Logistic (inland)')
 dev.off()
 
-
-
-# Posterior Distributions
+# Posterior Distributions ####
 load('../results_images/resABC_laplace.RData')
 tol=0.05
 library(coda)
@@ -148,7 +150,8 @@ abline(v=median(post$c),lty=2)
 dev.off()
 
 
-# Posterior Predictive Check
+
+# Posterior Predictive Check ####
 load('../results_images/predcheck_results.RData')
 load('../data/koreanC14.RData')
 library(rcarbon)
@@ -167,3 +170,74 @@ lines(observed$grid$calBP,observed$grid$PrDens,lwd=1)
 legend('topleft',legend=c('Observed','Median Posterior Predictive Check','95% Posterior Predictive Interval'),col=c(1,2,'lightgrey'),lwd=c(1,1,5),lty=c(1,2,1),cex=0.6,bg='white')
 dev.off()
 
+
+
+#  
+
+
+
+# Age-Depth Model of Kim 2004 ####
+
+# Read Kim 2004
+kim2004.dates = read.csv('../data/SSDP_102.Kim.2004-chron.csv',skip=1)
+kim2004.temp  = read.csv('../data/SSDP_102.Kim.2004.csv',skip=1)
+
+# Read marine20 calibration curve
+marine20 = read.csv('http://intcal.org/curves/marine20.14c', encoding="UTF-8",skip=11,header=F)
+createCalCurve(name='marine20',calAges=marine20[,1],uncalAges=marine20[,2],oneSigma=marine20[,3])
+file.copy(from = 'marine20.rda',to = system.file('data',package='Bchron'))
+
+# Fit Compound Poisson-Gamma chronology model
+# kim2004.model.marine13 = Bchronology(ages=round(kim2004.dates$T2L_SSDP_102_c14_date - kim2004.dates$T2L_SSDP_102_delta_r),ageSds=round(sqrt(kim2004.dates$T2L_SSDP_102_c14_1s_err^2+kim2004.dates$T2L_SSDP_102_delta_r_1s_error^2)),calCurves = rep('marine13',nrow(kim2004.dates)),ids=kim2004.dates$T2L_SSDP_102_labcode,positions=kim2004.dates$T2L_SSDP_102_depth_top,predictPositions=kim2004.temp$T2L_SSDP_102_depth)
+
+kim2004.model.marine20 = Bchronology(ages=round(kim2004.dates$T2L_SSDP_102_c14_date - kim2004.dates$T2L_SSDP_102_delta_r),ageSds=round(sqrt(kim2004.dates$T2L_SSDP_102_c14_1s_err^2+kim2004.dates$T2L_SSDP_102_delta_r_1s_error^2)),calCurves = rep('marine20',nrow(kim2004.dates)),ids=kim2004.dates$T2L_SSDP_102_labcode,positions=kim2004.dates$T2L_SSDP_102_depth_top,predictPositions=kim2004.temp$T2L_SSDP_102_depth)
+
+# Compare predicted ages
+# med.bchron.marine13 = apply(kim2004.model.marine13$thetaPredict,2,median)
+# med.bchron.marine20 = apply(kim2004.model.marine20$thetaPredict,2,median)
+# par(mfrow=c(2,2))
+# plot(kim2004.temp$T2L_SSDP_102_age_medianMedianBacon,med.bchron.marine13,pch=20)
+# abline(a=0,b=1,col='red')
+# plot(kim2004.temp$T2L_SSDP_102_age_medianMedianBacon,med.bchron.marine20,pch=20)
+# abline(a=0,b=1,col='red')
+
+# Plot Median Predicted Temperature Change
+pdf(file = "./figure_kim2004_reanalysis.pdf",width = 6,height = 5.5)
+layout(matrix(c(1,2,1,3),2,2))
+par(mar=c(5,4,1,1))
+plot(3000:7000,med,xlim=c(7000,3000),type='n',ylim=c(range(kim2004.temp$T2L_SSDP_102_uk37_SST_from_uk37)),xlab='cal BP',ylab='temperature (deg C)')
+rect(xleft=med.bchron.marine20[80],xright=med.bchron.marine20[81],ybottom=10,ytop=30,border=NA,col='lightblue')
+lines(med.bchron.marine20,kim2004.temp$T2L_SSDP_102_uk37_SST_from_uk37,type='l',lty=1,col='darkgrey')
+points(med.bchron.marine20[80:81],kim2004.temp$T2L_SSDP_102_uk37_SST_from_uk37[80:81],pch=20,col=1)
+text(4949.801,21.89769,'a')
+text(4716.733,20.45735,'b')
+lines(med.bchron.marine20[80:81],kim2004.temp$T2L_SSDP_102_uk37_SST_from_uk37[80:81],lwd=2,lty=2,col=1)
+
+point_a=kim2004.model.marine20$thetaPredict[,81]
+point_b=kim2004.model.marine20$thetaPredict[,80]
+
+dcool.hpdi.a=HPDinterval(mcmc(point_a),prob = 0.90)
+d.event.a=density(point_a)
+dcool.hpdi.b=HPDinterval(mcmc(point_b),prob = 0.90)
+d.event.b=density(point_b)
+
+plot(d.event.a$x,d.event.a$y,type='n',xlab='cal BP',ylab='Probability Density',axes=FALSE,xlim=c(5500,4000))
+legend('topright',legend='Timing of a',bty='n',cex=0.8)
+axis(1)
+axis(2)
+hpdi.x = d.event.a$x[which(d.event.a$x>=dcool.hpdi.a[1]&d.event.a$x<=dcool.hpdi.a[2])]
+hpdi.y = d.event.a$y[which(d.event.a$x>=dcool.hpdi.a[1]&d.event.a$x<=dcool.hpdi.a[2])]
+polygon(x=c(hpdi.x,rev(hpdi.x)),y=c(hpdi.y,rep(0,length(hpdi.y))),border=NA,col='lightblue')
+polygon(x=c(d.event.a$x,rev(d.event.a$x)),y=c(d.event.a$y,rep(0,length(d.event.a$y))))
+abline(v=median(point_a),lty=2)
+
+plot(d.event.b$x,d.event.b$y,type='n',xlab='cal BP',ylab='Probability Density',axes=FALSE,xlim=c(5500,4000))
+legend('topright',legend='Timing of b',bty='n',cex=0.8)
+axis(1)
+axis(2)
+hpdi.x = d.event.b$x[which(d.event.b$x>=dcool.hpdi.b[1]&d.event.b$x<=dcool.hpdi.b[2])]
+hpdi.y = d.event.b$y[which(d.event.b$x>=dcool.hpdi.b[1]&d.event.b$x<=dcool.hpdi.b[2])]
+polygon(x=c(hpdi.x,rev(hpdi.x)),y=c(hpdi.y,rep(0,length(hpdi.y))),border=NA,col='lightblue')
+polygon(x=c(d.event.b$x,rev(d.event.b$x)),y=c(d.event.b$y,rep(0,length(d.event.b$y))))
+abline(v=median(point_b),lty=2)
+dev.off()
