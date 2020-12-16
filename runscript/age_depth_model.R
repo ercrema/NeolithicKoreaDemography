8faf4cb5649f7fd6e51f4c8058599b193f451d33
Enrico R. Crema <enrico.crema@gmail.com>
2020-12-16 11:45:53 +0000

    Updated age-depth model, data, scripts, and results

--- runscript/age_depth_model.R
+++ runscript/age_depth_model.R
@@ -1,44 +1,46 @@
+## Load Required R Libraries
 library(Bchron)
 library(readxl)
 library(dplyr)
+
 ## Setup ####
 
 # Read Data
-kim2004.dates = read.csv('../data/kim2004/SSDP_102.Kim.2004-chron.csv',skip=1)
-kim2004.temp  = read.csv('../data/kim2004/SSDP_102.Kim.2004.csv',skip=1)
-constantine2020.dates = read.csv('../data/Constantine2020/pomaeho_c14.csv')
-constantine2020.apt = read_xlsx('../data/Constantine2020/pomaeho_pollen.xlsx',sheet = 'Sheet2')
+SSDP102.dates = read.csv('../data/Kim_etal_2004/SSDP102_Kim_etlal_2004_c14.csv',skip=1)
+SSDP102.temp  = read.csv('../data/Kim_etal_2004/SSDP102_Kim_etlal_2004_temp.csv',skip=1)
+pomaeho.dates = read.csv('../data/Constantine_etal_2020/pomaeho_constantine_etal_2020_c14.csv')
+pomaeho.apt = read.csv('../data/Constantine_etal_2020/pomaeho_constantine_etal_2020_aptp.csv')
+gy.dates = read.csv('../data/Park_etal_2019/GY_Park_etal_2019_c14.csv')
+gy.apt = read.csv('../data/Park_etal_2019/GY_Park_etal_2019_aptp.csv')
 
-
-## Constantine et al 2020, AP/T
+## Constantine et al 2020, AP/T ####
+## Delta R values calculated from the two dats at depth 1184 cm, using Reimar and Reimar 2016 calculation (DOI:10.1017/RDC.2016.117) and applet (http://calib.org/JS/JSdeltar20/)
 DeltaR = -213
 DeltaRError = 52
-constantine2020.dates = subset(constantine2020.dates,CRA>1000)
-constantine2020.dates$CRA[which(constantine2020.dates$Material2=='marine')]=constantine2020.dates$CRA[which(constantine2020.dates$Material2=='marine')]-DeltaR
-constantine2020.dates$CRAError[which(constantine2020.dates$Material2=='marine')]=round(sqrt(constantine2020.dates$CRAError[which(constantine2020.dates$Material2=='marine')]^2+DeltaRError^2))
-constantine2020.dates$calCurves = 'marine20'
-constantine2020.dates$calCurves[which(constantine2020.dates$Material2=='terrestrial')] = 'intcal20'
+pomaeho.dates = subset(pomaeho.dates,CRA>1000)
+pomaeho.dates$CRA[which(pomaeho.dates$Material2=='marine')]=pomaeho.dates$CRA[which(pomaeho.dates$Material2=='marine')]-DeltaR
+pomaeho.dates$CRAError[which(pomaeho.dates$Material2=='marine')]=round(sqrt(pomaeho.dates$CRAError[which(pomaeho.dates$Material2=='marine')]^2+DeltaRError^2))
+pomaeho.dates$calCurves = 'marine20'
+pomaeho.dates$calCurves[which(pomaeho.dates$Material2=='terrestrial')] = 'intcal20'
 
 outlierPresence = TRUE
-constantine2020.dates$OutlierProb = 0
-constantine2020.dates$Depth[nrow(constantine2020.dates)]=constantine2020.dates$Depth[nrow(constantine2020.dates)]+1
+pomaeho.dates$OutlierProb = 0
+pomaeho.dates$Depth[nrow(pomaeho.dates)]=pomaeho.dates$Depth[nrow(pomaeho.dates)]+1 #added artificial offset as Bchron does not handle two dates on the same depth
 
 
 set.seed(12345)
 while(outlierPresence)
 {
-  index = which(constantine2020.dates$OutlierProb<0.1)
-  constantine2020.model = Bchronology(ages=constantine2020.dates$CRA[index],ageSds=constantine2020.dates$CRAError[index],calCurves = constantine2020.dates$calCurves[index],ids=constantine2020.dates$LabCode[index],positions=constantine2020.dates$Depth[index],predictPositions=constantine2020.apt$Depth,jitterPositions = FALSE,iterations=50000, burn = 10000)
-  outliers=summary(constantine2020.model, type='outliers')
+  index = which(pomaeho.dates$OutlierProb<0.1)
+  pomaeho.model = Bchronology(ages=pomaeho.dates$CRA[index],ageSds=pomaeho.dates$CRAError[index],calCurves = pomaeho.dates$calCurves[index],ids=pomaeho.dates$LabCode[index],positions=pomaeho.dates$Depth[index],predictPositions=pomaeho.apt$Depth,jitterPositions = FALSE,iterations=50000, burn = 10000)
+  outliers=summary(pomaeho.model, type='outliers')
   if (any(outliers$OutlierProb>0.1))
   {
-    constantine2020.dates$OutlierProb[match(outliers$Date,constantine2020.dates$LabCode)]=outliers$OutlierProb
+    pomaeho.dates$OutlierProb[match(outliers$Date,pomaeho.dates$LabCode)]=outliers$OutlierProb
   } else {outlierPresence=FALSE}
 }
 
-## Kim et al 2004, Alkenone
-
-
+## Kim et al 2004, Alkenone ####
 # Delta R (From http://calib.org/marine/index.html?npoints=1&clat=34.72845262222496&clon=128.2532094998296)
 deltaRs = c(-94,-71)
 deltaRSigmas = c(22,24)
@@ -48,25 +50,43 @@ deltaR.weightedMean = round(sum(deltaRs/deltaRSigmas)/sum(1/deltaRSigmas))
 deltaRerr = round(sqrt(((1/(n-1)) * sum(((deltaRs-mean(deltaRs))/deltaRSigmas)^2))/((1/(n-1))*sum((1/deltaRSigmas)^2))))
 
 outlierPresence = TRUE
-kim2004.dates$OutlierProb = 0
+SSDP102.dates$OutlierProb = 0
 
 set.seed(12345)
 while(outlierPresence)
 {
-  index = which(kim2004.dates$OutlierProb<0.1)
-  kim2004.model = Bchronology(ages=round(kim2004.dates$T2L_SSDP_102_c14_date - deltaR.weightedMean)[index],ageSds=round(sqrt(kim2004.dates$T2L_SSDP_102_c14_1s_err^2+deltaRerr^2))[index],calCurves = rep('marine20',nrow(kim2004.dates))[index],ids=kim2004.dates$T2L_SSDP_102_labcode[index],positions=kim2004.dates$T2L_SSDP_102_depth_top[index],predictPositions=kim2004.temp$T2L_SSDP_102_depth,iterations=50000, burn = 10000)
-  outliers=summary(kim2004.model, type='outliers')
+  index = which(SSDP102.dates$OutlierProb<0.1)
+  SSDP102.model = Bchronology(ages=round(SSDP102.dates$T2L_SSDP_102_c14_date - deltaR.weightedMean)[index],ageSds=round(sqrt(SSDP102.dates$T2L_SSDP_102_c14_1s_err^2+deltaRerr^2))[index],calCurves = rep('marine20',nrow(SSDP102.dates))[index],ids=SSDP102.dates$T2L_SSDP_102_labcode[index],positions=SSDP102.dates$T2L_SSDP_102_depth_top[index],predictPositions=SSDP102.temp$T2L_SSDP_102_depth,iterations=50000, burn = 10000)
+  outliers=summary(SSDP102.model, type='outliers')
   if (any(outliers$OutlierProb>0.1))
   {
-    kim2004.dates$OutlierProb[match(outliers$Date,kim2004.dates$T2L_SSDP_102_labcode)]=outliers$OutlierProb
+    SSDP102.dates$OutlierProb[match(outliers$Date,SSDP102.dates$T2L_SSDP_102_labcode)]=outliers$OutlierProb
+  } else {outlierPresence=FALSE}
+}
+
+## Park et al 2019, AP/TP ####
+outlierPresence = TRUE
+gy.dates$OutlierProb = 0
+while(outlierPresence)
+{
+  index = which(gy.dates$OutlierProb<0.1)
+  gy.model = Bchronology(ages=gy.dates$CRA[index],ageSds=gy.dates$CRAError[index],calCurves = rep('intcal20',nrow(gy.dates))[index],ids=gy.dates$LabCode[index],positions=gy.dates$Depth[index],predictPositions=gy.apt$Depth,iterations=50000, burn = 10000)
+  outliers=summary(gy.model, type='outliers')
+  if (any(outliers$OutlierProb>0.1))
+  {
+    gy.dates$OutlierProb[match(outliers$Date,gy.dates$LabCode)]=outliers$OutlierProb
   } else {outlierPresence=FALSE}
 }
 
 
-# Extract Median Dates
-med.kim2004.model= apply(kim2004.model$thetaPredict,2,median)
-med.constantine2020.model= apply(constantine2020.model$thetaPredict,2,median)
 
-save(med.kim2004.model,med.constantine2020.model,kim2004.model,constantine2020.model,kim2004.dates,kim2004.temp,constantine2020.dates,constantine2020.apt,file='../R_image_files/agedepthmodels.RData')
+
+# Extract Median Dates
+med.pomaeho.model= apply(pomaeho.model$thetaPredict,2,median)
+med.SSDP102.model= apply(SSDP102.model$thetaPredict,2,median)
+med.gy.model= apply(gy.model$thetaPredict,2,median)
+
+
+save(med.pomaeho.model,med.SSDP102.model,med.gy.model,pomaeho.apt,pomaeho.dates,pomaeho.model,SSDP102.dates,SSDP102.temp,SSDP102.model,gy.dates,gy.apt,gy.model,file='../R_image_files/agedepthmodels.RData')
 
 
