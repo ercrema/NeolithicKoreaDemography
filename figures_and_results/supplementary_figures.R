b30a0b83627face957fb152bef17c521e5aa4624
Enrico R. Crema <enrico.crema@gmail.com>
2021-01-21 10:55:26 +0000

    Revised supplementary figures with age-depth models

--- figures_and_results/supplementary_figures.R
+++ figures_and_results/supplementary_figures.R
@@ -15,9 +15,9 @@ load(here('R_image_files','koreanC14.RData'))
 load(here('R_image_files','spd_test_results.RData'))
 load(here('R_image_files','agedepthmodels.RData'))
 load(here('R_image_files','mcmcdiagnostic_postpredcheck.RData'))
-load(here('R_image_files/','mcmc_samples_coastal.RData'))
-load(here('R_image_files/','mcmc_samples_inland.RData'))
-load(here('R_image_files/','mcmc_samples_all.RData'))
+load(here('R_image_files','mcmc_samples_coastal.RData'))
+load(here('R_image_files','mcmc_samples_inland.RData'))
+load(here('R_image_files','mcmc_samples_all.RData'))
 
 
 ## Figure S1 - Millet Dates ####
@@ -55,8 +55,8 @@ rect(xleft=med.SSDP102.model[80],xright=med.SSDP102.model[81],ybottom=10,ytop=30
 lines(med.SSDP102.model,SSDP102.temp$T2L_SSDP_102_uk37_SST_from_uk37,type='l',lty=1,col='darkgrey')
 points(med.SSDP102.model,SSDP102.temp$T2L_SSDP_102_uk37_SST_from_uk37,pch=20,col='darkgrey')
 points(med.SSDP102.model[80:81],SSDP102.temp$T2L_SSDP_102_uk37_SST_from_uk37[80:81],pch=20,col=1,cex=1.2)
-text(4949.801,22,TeX('$1050cm \\, (a_{1})$'),cex=0.8)
-text(4700.733,20.33,TeX('$1035cm \\, (a_{2})$'),cex=0.8)
+text(4949.801,22,TeX('$1050cm \\, (d_{1})$'),cex=0.8)
+text(4700.733,20.33,TeX('$1035cm \\, (d_{2})$'),cex=0.8)
 axis(2)
 par(mar=c(2.5,4,1,1))
 plot(0,xlim=c(7000,3000),type='n',ylim=c(0.75,0.95),xlab='cal BP',ylab='AP/TP',axes=F,main='b')
@@ -64,8 +64,8 @@ rect(xleft=med.pomaeho.model[72],xright=med.pomaeho.model[73],ybottom=0,ytop=1,b
 lines(med.pomaeho.model,pomaeho.apt$AP_T_Ratio,type='l',lty=1,col='darkgrey')
 points(med.pomaeho.model,pomaeho.apt$AP_T_Ratio,pch=20,col='darkgrey')
 points(med.pomaeho.model[72:73],pomaeho.apt$AP_T_Ratio[72:73],pch=20,col=1,cex=1.2)
-text(4348.611,0.95,TeX('$1020cm \\, (b_{1})$'),cex=0.8)
-text(4262,0.76,TeX('$1017cm \\, (b_{2})$'),cex=0.8)
+text(4348.611,0.95,TeX('$1020cm \\, (f_{1})$'),cex=0.8)
+text(4262,0.76,TeX('$1017cm \\, (f_{2})$'),cex=0.8)
 axis(2)
 par(mar=c(2.5,4,1,1))
 plot(0,xlim=c(7000,3000),type='n',ylim=c(0.75,0.95),xlab='cal BP',ylab='AP/TP',axes=F,main='b')
@@ -73,16 +73,31 @@ rect(xleft=med.gy.model[78],xright=med.gy.model[79],ybottom=0.76,ytop=1,border=N
 lines(med.gy.model,gy.apt$AP_TP_Ratio,type='l',lty=1,col='darkgrey')
 points(med.gy.model,gy.apt$AP_TP_Ratio,pch=20,col='darkgrey')
 points(med.gy.model[78:79],gy.apt$AP_TP_Ratio[78:79],pch=20,col=1,cex=1.2)
-text(4698,0.9,TeX('$890cm \\, (c_{1})$'),cex=0.8)
-text(4500,0.82,TeX('$887cm \\, (c_{2})$'),cex=0.8)
+text(4698,0.9,TeX('$890cm \\, (g_{1})$'),cex=0.8)
+text(4500,0.82,TeX('$887cm \\, (g_{2})$'),cex=0.8)
 axis(2)
 axis(1,at=seq(7000,3000,-1000),line=-1)
 axis(1,at=seq(7000,3000,-500),labels=NA,line=-1)
 axis(1,at=seq(7000,3000,-100),tck=-0.01,labels=NA,line=-1)
 mtext('Cal BP',side=1,line=1.5,cex=0.8)
 dev.off()
-## Figure S5 - Posterior Events  ####
-pdf(file = here('figures_and_results','figureS5.pdf'),width = 7,height = 10)
+## Figure S5 Age-Dept Model Kim et al ####
+pdf(file = here('figures_and_results','figureS5.pdf'),width = 9.5,height = 4.5)
+plot(SSDP102.model)
+dev.off()
+
+## Figure S6 Age-Dept Model Pomaeho ####
+pdf(file = here('figures_and_results','figureS6.pdf'),width = 9.5,height = 4.5)
+plot(pomaeho.model)
+dev.off()
+
+## Figure S7 Age-Dept Model GY ####
+pdf(file = here('figures_and_results','figureS7.pdf'),width = 9.5,height = 4.5)
+plot(gy.model)
+dev.off()
+
+## Figure S8 - Posterior Events  ####
+pdf(file = here('figures_and_results','figureS8.pdf'),width = 7,height = 10)
 
 plotEventPosterior = function(x,hpd,legloc='topright',legsize=1,...)
 {
@@ -116,14 +131,14 @@ plotEventPosterior(x=point_c2,hpd=0.90,xlim=c(5500,3500),main=TeX('$Event \\, c_
 dev.off()
 
 
-## Figure S6 - Prior Predictive Check ####
-pdf(file = here('figures_and_results','figureS6.pdf'),width = 5,height = 5)
+## Figure S9 - Prior Predictive Check ####
+pdf(file = here('figures_and_results','figureS9.pdf'),width = 5,height = 5)
 prior.params=list(r1=rnorm(500,mean=0,sd=0.0004),r2=rnorm(500,mean=0,sd=0.0004),mu=rtruncnorm(500,a=3000,b=7000,mean=5000,sd=1000))
 modelPlot(model=dDoubleExponentialGrowth,params = prior.params,a = 7000,b=3000, nsample = 500,alpha=0.1,main='Prior Predictive Check')
 dev.off()
 
-## Figure S7 - Trace Plot ####
-pdf(file = here('figures_and_results','figureS7.pdf'),width = 7.5,height = 7.5)
+## Figure S10 - Trace Plot ####
+pdf(file = here('figures_and_results','figureS10.pdf'),width = 7.5,height = 7.5)
 par(mfcol=c(3,3),mar=c(4.5,5.5,2,1))
 plot(as.numeric(mcmc.samples.coastal$chain1[,'r1']),type='l',ylim=range(params.coastal$r1),lwd=0.3,col='lightblue',xlab='MCMC Samples',ylab='',las=1,main=TeX('$r_1\\, Coastal$'))
 lines(as.numeric(mcmc.samples.coastal$chain2[,'r1']),col='pink',lwd=0.3)
@@ -166,17 +181,17 @@ lines(as.numeric(mcmc.samples.all$chain3[,'chp']),col='grey',lwd=0.3)
 mtext(TeX('$c$'),2,line=4,cex=0.7,las=2)
 
 dev.off()
-## Figure S8 - Fitted Model ####
-pdf(file = here('figures_and_results','figureS8.pdf'),width = 7,height = 3)
+## Figure S11 - Fitted Model ####
+pdf(file = here('figures_and_results','figureS11.pdf'),width = 7,height = 3)
 par(mfrow=c(1,3))
 modelPlot(dDoubleExponentialGrowth,params =params.coastal,nsample=500,alpha=0.05,a=7000,b=3000,main='a')
 modelPlot(dDoubleExponentialGrowth,params =params.inland,nsample=500,alpha=0.05,a=7000,b=3000,main='b')
 modelPlot(dDoubleExponentialGrowth,params =params.all,nsample=500,alpha=0.05,a=7000,b=3000,main='c')
 dev.off()
 
-## Figure S9 - Posterior Marginal Distributions ####
+## Figure S12 - Posterior Marginal Distributions ####
 options(scipen=9999)
-pdf(file = here('figures_and_results','figureS9.pdf'),width = 7.5,height = 7.5)
+pdf(file = here('figures_and_results','figureS12.pdf'),width = 7.5,height = 7.5)
 par(mfrow=c(3,3),mar=c(5,5,2,1))
 postHPDplot(params.coastal$r1*100,xlab='Annual Growth Rate (%)',ylab='Probability',main=TeX('$r_1\\,Coastal$'))
 postHPDplot(params.coastal$r2*100,xlab='Annual Growth Rate (%)',ylab='Probability',main=TeX('$r_2\\,Coastal$'))
@@ -192,8 +207,8 @@ dev.off()
 
 
 
-## Figure S10 - Posterior Predictive Checks ####
-pdf(file = here('figures_and_results','figureS10.pdf'),width = 5,height = 6)
+## Figure S13 - Posterior Predictive Checks ####
+pdf(file = here('figures_and_results','figureS13.pdf'),width = 5,height = 6)
 par(mfrow=c(3,1),mar=c(5,4,2,0.5))
 plot(params.coastal.uncalsample,interval = 0.95,main='Coastal')
 plot(params.inland.uncalsample,interval = 0.95,main='Inland')
