fd3d821e703c1d66d3655753651af75645c7138f
Enrico R. Crema <enrico.crema@gmail.com>
2021-01-25 11:20:15 +0000

    Revised NHST scripts and results

--- runscript/statistical_tests.R
+++ runscript/statistical_tests.R
@@ -1,12 +1,12 @@
-# Load Files
-load('../R_image_files/koreanC14.RData')
+# Load Libraries
+library(here)
 library(rcarbon)
+load(here('R_image_files','koreanC14.RData'))
+
 # Core Parameters
 runm = 100 #smoothing window
 nsim = 1000 #number of MC iterations for modelTest()
 timeRange = c(7000,3000) #time range of analysis
-ncores = 6 #number of cores
-bw=100 # tempoeral kernel bandwidth
 # Inland/Coastal Index ####
 coastal.index = which(koreaC14$region=='coastal')
 inland.index = which(koreaC14$region=='inland')
@@ -20,53 +20,6 @@ koreaC14$milletAsso[is.na(koreaC14$milletAsso)]='no'
 allDates=calibrate(koreaC14$c14age,koreaC14$c14error)
 millet.permtest=permTest(allDates,marks=koreaC14$milletAsso,timeRange=timeRange,runm=runm,nsim=nsim)
 
-# Composite Kernel Density Estimate with Bootstrapping ####
-s.coastal = sampleDates(caldates[which(koreaC14$region=='coastal')],bins=bins[which(koreaC14$region=='coastal')],nsim=1000,boot=FALSE)
-s.inland = sampleDates(caldates[which(koreaC14$region=='inland')],bins=bins[which(koreaC14$region=='inland')],nsim=1000,boot=FALSE)
-ckde.coastal = ckde(s.coastal,timeRange=c(7000,3000),bw=bw)
-ckde.inland = ckde(s.inland,timeRange=c(7000,3000),bw=bw)
-
-
-# Theoretical Growth Model Tests ####
-
-## Exponential ##
-set.seed(123)
-exp.general.test <- modelTest(x=caldates,bins=bins,errors=koreaC14$c14error,model="exponential",runm=runm,nsim=nsim,ncores=ncores,timeRange=timeRange)
-
-exp.coastal.test <- modelTest(x=caldates[coastal.index],bins=bins[coastal.index],errors=koreaC14$c14error[coastal.index],model="exponential",runm=runm,nsim=nsim,ncores=ncores,timeRange=timeRange)
-
-exp.inland.test <- modelTest(x=caldates[inland.index],bins=bins[inland.index],errors=koreaC14$c14error[inland.index],model="exponential",runm=runm,nsim=nsim,ncores=ncores,timeRange=timeRange)
-
-
-## Logistic ##
-spd.general <- spd(x=caldates,timeRange=timeRange,bins=bins,runm=runm,spdnormalised = TRUE)
-spd.coastal <- spd(x=caldates[coastal.index],timeRange=timeRange,bins=bins[coastal.index],runm=runm,spdnormalised = TRUE)
-spd.inland <- spd(x=caldates[inland.index],timeRange=timeRange,bins=bins[inland.index],runm=runm,spdnormalised = TRUE)
-
-obs.calBP <- timeRange[1]:timeRange[2]
-prdens.general <- spd.general$grid$PrDens
-prdens.coastal <- spd.coastal$grid$PrDens
-prdens.inland <- spd.inland$grid$PrDens
-
-logFit.general <- nls(prdens.general~SSlogis(obs.calBP, Asym, xmid, scale),control=nls.control(maxiter=200),start=list(Asym=0.2,xmid=5500,scale=-100))
-logFit.coastal <- nls(prdens.coastal~SSlogis(obs.calBP, Asym, xmid, scale),control=nls.control(maxiter=200),start=list(Asym=0.2,xmid=5500,scale=-100))
-logFit.inland<- nls(prdens.inland~SSlogis(obs.calBP, Asym, xmid, scale),control=nls.control(maxiter=200),start=list(Asym=0.2,xmid=5500,scale=-100))
-
-logistic.general=data.frame(calBP=obs.calBP,PrDens=SSlogis(obs.calBP,coefficients(logFit.general)[1],coefficients(logFit.general)[2],coefficients(logFit.general)[3]))
-logistic.coastal=data.frame(calBP=obs.calBP,PrDens=SSlogis(obs.calBP,coefficients(logFit.coastal)[1],coefficients(logFit.coastal)[2],coefficients(logFit.coastal)[3]))
-logistic.inland=data.frame(calBP=obs.calBP,PrDens=SSlogis(obs.calBP,coefficients(logFit.inland)[1],coefficients(logFit.inland)[2],coefficients(logFit.inland)[3]))
-
-logistic.general.test <- modelTest(caldates,bins=bins,errors=koreaC14$c14error,model="custom",runm=runm,nsim=nsim,ncores=ncores,timeRange=timeRange,predgrid=logistic.general,spdnormalised = TRUE)
-logistic.coastal.test <- modelTest(caldates[coastal.index],bins=bins[coastal.index],errors=koreaC14$c14error[coastal.index],model="custom",runm=runm,nsim=nsim,ncores=ncores,timeRange=timeRange,predgrid=logistic.general,spdnormalised = TRUE)
-logistic.inland.test <- modelTest(caldates[inland.index],bins=bins[inland.index],errors=koreaC14$c14error[inland.index],model="custom",runm=runm,nsim=nsim,ncores=ncores,timeRange=timeRange,predgrid=logistic.general,spdnormalised = TRUE)
-
-# Save Output Image #####
-
-# assign NULL to fitobject to reduce image size
-exp.general.test$fitobject=NULL
-exp.coastal.test$fitobject=NULL
-exp.inland.test$fitobject=NULL
-
-save(coastal.inland.permtest,millet.permtest,ckde.coastal,ckde.inland,exp.general.test,exp.coastal.test,exp.inland.test,logistic.general.test,logistic.coastal.test,logistic.inland.test,file='../R_image_files/spd_test_results.RData')
+save(coastal.inland.permtest,millet.permtest,file=here('R_image_files','spd_test_results.RData'))
 
 
